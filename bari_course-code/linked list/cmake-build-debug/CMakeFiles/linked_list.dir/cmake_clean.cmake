b49811c410e64b3f4743e50c2e12a1583d254d5f
rishusingh022 <rishusingh022@gmail.com>
2020-11-05 23:01:51 +0530

    added course exercise code

---
+++ bari_course-code/linked list/cmake-build-debug/CMakeFiles/linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linked_list.dir/concatenationoFlinkedlist.c.obj"
+  "liblinked_list.dll.a"
+  "linked_list.exe"
+  "linked_list.exe.manifest"
+  "linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
